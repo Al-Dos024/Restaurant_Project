a8f987de8a58217a20354e3f9f27386cf5914d43
Al-Dos024 <amjdos0190edu@gmail.com>
2023-09-10 19:34:51 +0300

    small change

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import '1-Onboarding.dart';
+import '';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
